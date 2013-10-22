19b72f32df6fd30e1a449962ed4c2c362304d303
LinhIT2013 <linhit2013@gmail.com>
2013-10-22 10:47:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,24 +2,28 @@
 <script type="text/javascript" src="http://code.jquery.com/jquery-2.0.3.js"></script>
 <script> 
 $(document).ready(function(){ 
-$.ajax({ 
+  $.ajax({ 
   type: "GET",//su dung phuong thuc get sub mit lem 
-  url: "nhatnghe.xml",//doc trang nhat nghe.xml 
+  url: "data.xml",//doc trang nhat nghe.xml 
   dataType: "xml",// khai bao voi trinh duyet dang du lieu la xml 
   success: function(xml) {//du lieu tra ve do vao bien xml 
          //xu ly dữ liệu dổ về 
          $(xml).find('khaigiang').each(function(){//tim các element khaigiang trong data xml  
      		var id = $(this).attr('id');//truy xuất vao doi tuong id  va gan vao bien id 
-      	var lop = $(this).find('lop').text();//tim cac element lop trong doi tuong hien hanh 
-      	var gia = $(this).find('gia').text();//tim cac element gia trong doi tuong hien hanh 
-      	var data = "<div>";//tao 1 the div de do du lieu ra 
-      	data +=     "<p>"+lop + "</p>";       
-      	data +=     "<p>"+ gia + "</p>";       
-      	data += "</div>";//alert(data); 
-      	$(data).appendTo('#wrap'); 
+      	var time = $(this).find('time').text();//tim cac element lop trong doi tuong hien hanh 
+      	vartieude = $(this).find('tieude').text();
+      	var note = $(this).find('note').text();//tim cac element gia trong doi tuong hien hanh 
+      	var kieubao = $(this).find('kieubao').text();
+      	var thoigianbao = $(this).find('thoigianbao').text();
+        	//xuat du lieu
+        	var data = "<div>";//tao 1 the div de do du lieu ra 
+      	  data +=     "<p>"+lop + "</p>";       
+      	  data +=     "<p>"+ gia + "</p>";       
+        	data += "</div>";//alert(data); 
+      	  $(data).appendTo('#wrap'); 
     		}); 
-  } 
-}); 
+        } 
+    }); 
 }); 
 </script>
 
